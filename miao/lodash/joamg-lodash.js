a95f8e7e9cc83179da14f0dae8bddac904583723
Yoha Sakura <sakura@mbpa.lan>
2021-07-12 15:44:31 +0800

    update

--- miao/lodash/joamg-lodash.js
+++ miao/lodash/joamg-lodash.js
@@ -1,15 +1,66 @@
 var joamg = function () {
-  function chunk () {
+  var a = []
+  function chunk (array, size) {
+    if (array.length < size) {
+      return array
+    } else {
+      for (var i = 0; i < array.length; i++) {
 
+        
+      }
+    }
   }
 
-  function compact() {
 
+  function compact(array) {
+    var a = []
+      for(i = 0; i < array.length; i++) {
+        if ((typeof array[i] !== String) && Number(array[i]) == parseFloat(array[i])) {
+          a.push(array[i])
+        }
+      }
+      return a
   }
+
+
+  function flattenDeep(array) {
+    for(i = 0; i < array.length; i++) {
+      if(array.length == 1 ) return array
+      else 
+      ()
+    }
+  }
+
+
+
+
+
+
+
+
 
   return {
     chunk: chunk,
-    compact: compact,
+    compact: a,
+    unique: unique,
+    uniqueBy: uniqueBy,
+    flattenDeep: flattenDeep,
+    Depth:Depth,
+    gruopBy:gruopBy,
+    keyB:keyB,
+    forEach: forEach,
+    map:map,
+    filter:filter,
+    reduce:reduce,
+    zip:zip,
+    unzip:unzip,
+    forEach:forEach,
+    map:map,
+    filter:filter,
+    reduce:reduce,
+    zip:zip,
+    unz:unz,
+ 
   }
   
 }
